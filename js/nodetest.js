11a13d76f77bc39d0cf6ed224f3dd73c96f1a283
41off <58831292+41off@users.noreply.github.com>
2020-12-12 08:54:41 -0500

    Add files via upload

---
+++ js/nodetest.js
@@ -0,0 +1,17 @@
+﻿var fs = require('fs');
+var path = require('path');
+var dirPath = process.argv['Replay/Video'];  //directory path
+var fileType = '.'+process.argv['.mkv']; //file extension
+var files = [];
+fs.readdir(dirPath, function(err,list){
+    if(err) throw err;
+    for(var i=0; i<list.length; i++)
+    {
+        if(path.extname(list[i])===fileType)
+        {
+            console.log(list[i]); //print the file
+            files.push(list[i]); //store the file name into the array files
+        }
+    }
+});
+
