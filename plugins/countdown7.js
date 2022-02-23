2e6ad29948fcbf78a7829f8315fcf2cd091158e4
41off <58831292+41off@users.noreply.github.com>
2022-02-23 17:30:21 -0500

    Add files via upload

--- plugins/countdown7.js
+++ plugins/countdown7.js
@@ -1,5 +1,5 @@
 ﻿
-function getTimeRemaining4(endtime) {
+function getTimeRemaining7(endtime) {
   const total = Date.parse(endtime) - Date.parse(new Date());
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
@@ -15,15 +15,15 @@ function getTimeRemaining4(endtime) {
   };
 }
 
-function initializeClock4(id, endtime) {
+function initializeClock7(id, endtime) {
   const clock = document.getElementById(id);
   const daysSpan = clock.querySelector('.days');
   const hoursSpan = clock.querySelector('.hours');
   const minutesSpan = clock.querySelector('.minutes');
   const secondsSpan = clock.querySelector('.seconds');
 
-function updateClock4() {
-    const t = getTimeRemaining4(endtime);
+function updateClock7() {
+    const t = getTimeRemaining7(endtime);
 
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
@@ -31,14 +31,15 @@ function updateClock4() {
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
-      clearInterval(timeinterval3);
+      clearInterval(timeinterval7);
     }
   }
 
-  updateClock4();
-  const timeinterval4 = setInterval(updateClock4, 1000);
+  updateClock7();
+  const timeinterval7 = setInterval(updateClock7, 1000);
 }
 
-const deadline4 = 'July 8 2022 08:00:00 GMT-0500';
+const deadline7 = 'March 09 2022 08:00:00 GMT+1100';
 
-initializeClock4('clockdiv4', deadline4);
+initializeClock7('clockdiv7', deadline7);
+
